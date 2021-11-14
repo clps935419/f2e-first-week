d375f0bcb434cd6a31c88975c25a7d1729b0b4b3
clps935419 <clps935419@gmail.com>
2021-11-14 17:37:55 +0800

    20211114_縣市未完

---
+++ src/components/Home/City.vue
@@ -0,0 +1,102 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    setup(){
+    const tmpUrl = 
+    '@/img/city/';
+
+    const city =[
+        {
+            name:'台北市',
+            height:'L',
+            img:`${tmpUrl}taipei.png`
+        },
+        {
+            name:'新北市',
+            height:'S',
+            img:`${tmpUrl}new_taipe.png`
+        },
+        {
+            name:'桃園市',
+            height:'S',
+            img:`${tmpUrl}touyuan.png`
+        },
+        {
+            name:'新竹市',
+            height:'L',
+            img:`${tmpUrl}shinzhu.png`
+        },
+        {
+            name:'台    中',
+            height:'S',
+            img:`${tmpUrl}taizhung.png`
+
+        },
+        {
+            name:'南    投',
+            height:'S',
+            img:`${tmpUrl}nantou.png`
+
+        },
+        {
+            name:'嘉    義',
+            height:'L',
+            img:`${tmpUrl}zhiaiee.png`
+
+        },
+        {
+            name:'台    南',
+            height:'L',
+            img:`${tmpUrl}tainan.png`
+
+        },
+        {
+            name:'高    雄',
+            height:'S',
+            img:`${tmpUrl}kaoshung.png`
+
+        },
+        {
+            name:'屏    東',
+            height:'S',
+            img:`${tmpUrl}pintaung.png`
+
+        },
+        {
+            name:'宜    蘭',
+            height:'L',
+            img:`${tmpUrl}ialan.png`
+
+        },
+        {
+            name:'花    蓮',
+            height:'S',
+            img:`${tmpUrl}houalian.png`
+
+        },
+        {
+            name:'台    東',
+            height:'S',
+            img:`${tmpUrl}taitaung.png`
+
+        },
+        {
+            name:'金門馬祖.澎湖',
+            height:'L',
+            img:`${tmpUrl}outside.png`
+
+        },
+    ]
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
