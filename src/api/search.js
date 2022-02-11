2430ad7cf0f8f3894c7b8f939a7291c6b579f42d
clps935419 <clps935419@gmail.com>
2022-02-11 16:40:06 +0800

    修API filter

--- src/api/search.js
+++ src/api/search.js
@@ -8,7 +8,7 @@ export const getSpotSearch = (parms) => {
     //https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$filter=contains(Name%2C'%E9%BB%9E')%20and%20contains(Address%2C'%E5%B1%8F%E6%9D%B1')&$top=30&$format=JSON
 
     return activityRequest.get(
-        `/MOTC/v2/Tourism/ScenicSpot?$filter=contains(Name%2C'${parms.searchText}')%20and%20contains(Address%2C'${parms.searchLocate}')and%20Picture%2FPictureUrl1%20ne%20null&$top=30&$format=JSON`,
+        `/MOTC/v2/Tourism/ScenicSpot?$filter=contains(ScenicSpotName%2C'${parms.searchText}')%20and%20contains(Address%2C'${parms.searchLocate}')and%20Picture%2FPictureUrl1%20ne%20null&$top=30&$format=JSON`,
         {
             header: useAuthorizationHeader(),
         }
@@ -18,7 +18,7 @@ export const getSpotSearch = (parms) => {
 export const getRestaurantSearch = (parms) => {
     
     return activityRequest.get(
-        `/MOTC/v2/Tourism/Restaurant?$filter=contains(Name%2C'${parms.searchText}')%20and%20contains(Address%2C'${parms.searchLocate}')and%20Picture%2FPictureUrl1%20ne%20null&$top=30&$format=JSON`,
+        `/MOTC/v2/Tourism/Restaurant?$filter=contains(RestaurantName%2C'${parms.searchText}')%20and%20contains(Address%2C'${parms.searchLocate}')and%20Picture%2FPictureUrl1%20ne%20null&$top=30&$format=JSON`,
         {
             header: useAuthorizationHeader(),
         }
@@ -29,7 +29,7 @@ export const getHotelSearch = (parms) => {
     //https://ptx.transportdata.tw/MOTC/v2/Tourism/Hotel?$filter=contains(Name%2C'%E9%BB%9E')%20and%20contains(Address%2C'%E5%B1%8F%E6%9D%B1')and%20Picture%2FPictureUrl1%20ne%20null&$top=30&$format=JSON
 
     return activityRequest.get(
-        `/MOTC/v2/Tourism/Hotel?$filter=contains(Name%2C'${parms.searchText}')%20and%20contains(Address%2C'${parms.searchLocate}')and%20Picture%2FPictureUrl1%20ne%20null&$top=30&$format=JSON`,
+        `/MOTC/v2/Tourism/Hotel?$filter=contains(HotelName%2C'${parms.searchText}')%20and%20contains(Address%2C'${parms.searchLocate}')and%20Picture%2FPictureUrl1%20ne%20null&$top=30&$format=JSON`,
         {
             header: useAuthorizationHeader(),
         }
@@ -38,7 +38,7 @@ export const getHotelSearch = (parms) => {
 //活動搜尋
 export const getActiveSearch = (parms) => {
     return activityRequest.get(
-        `/MOTC/v2/Tourism/Activity?$filter=contains(Name%2C'${parms.searchText}')%20and%20contains(Address%2C'${parms.searchLocate}')and%20Picture%2FPictureUrl1%20ne%20null&$top=30&$format=JSON`,
+        `/MOTC/v2/Tourism/Activity?$filter=contains(ActivityName%2C'${parms.searchText}')%20and%20contains(Address%2C'${parms.searchLocate}')and%20Picture%2FPictureUrl1%20ne%20null&$top=30&$format=JSON`,
         {
             header: useAuthorizationHeader(),
         }
